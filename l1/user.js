c983977998849928bd63650ab5ee4398fad040f8
Maksim Gogolev <mgglev@gmail.com>
2016-07-22 21:48:58 +0300

    use json

--- l1/user.js
+++ l1/user.js
@@ -1,9 +1,10 @@
+var phrases = require('./ru');
 function User(name) {
     this.name=name;
 }
 
 User.prototype.hello = function (who) {
-    console.log("Hey, " + who.name);
+    console.log(phrases.Hey +", " + who.name);
 };
 
 console.log("user.js is required");
